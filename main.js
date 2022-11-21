17c42178edfbd121b5792a1bcd0f9bd97830c5cb
Benjamin Hughes <behu@kea.dk>
2022-11-21 13:04:12 +0100

    asd

---
+++ main.js
@@ -0,0 +1,26 @@
+//console.log(1)
+
+fetch('http://localhost:3000/api/wishes')
+    .then(response => response.json())
+    .then(function (wishes) {
+        renderWishes(wishes);
+    });
+
+function renderWishes(wishes) {
+    console.log(wishes);
+    const ul = document.querySelector('ul#wishes');
+    console.log(ul);
+    for (let i = 0; i < wishes.length; i++) {
+        const wish = wishes[i];
+        console.log(wish);
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <p class="text">${wish.text}</p>
+            <p class="price">${wish.price}</p>
+        `;
+        ul.appendChild(li);
+    }
+}
+
+
+
