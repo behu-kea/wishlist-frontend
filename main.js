c6db1d9dcb2083b947050669365a4949fe3fd9f7
Benjamin Hughes <behu@kea.dk>
2022-11-22 09:20:44 +0100

    asd

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-// ====== CONFIG & GLOBAL VARS ====== //
-const isLocalhost = location.hostname === "localhost" || location.hostname === "127.0.0.1";
-const serverUrl = "https://wishlist-fzb7.onrender.com";
-const endpoint = isLocalhost ? "http://localhost:3000" : serverUrl;
-console.log(isLocalhost);
-fetch(`${endpoint}/api/wishes`)
+fetch(`http://localhost:3000/api/wishes`)
     .then(response => response.json())
     .then(function (wishes) {
         renderWishes(wishes);
+        registerEvents(wishes);
     });
 
 function renderWishes(wishes) {
     console.log(wishes);
     const ul = document.querySelector('ul#wishes');
+    ul.innerHTML = '';
     console.log(ul);
     for (let i = 0; i < wishes.length; i++) {
         const wish = wishes[i];
@@ -23,6 +20,36 @@ function renderWishes(wishes) {
         `;
         ul.appendChild(li);
     }
+}
+
+function registerEvents(wishes) {
+    console.log(wishes);
+    const button = document.querySelector('button.filter');
+    console.log(button);
+    button.addEventListener('click', function() {
+        console.log('button clicked');
+        const inputElement = document.querySelector('input');
+        console.log(inputElement);
+        const minimumPrice = inputElement.value;
+        console.log(minimumPrice);
+        const filteredWishes = [];
+        for (let i = 0; i < wishes.length; i++) {
+            const wish = wishes[i];
+            console.log(wish.price);
+            console.log(minimumPrice);
+            console.log(wish.price > minimumPrice);
+            if(parseFloat(wish.price) > parseFloat(minimumPrice)) {
+                filteredWishes.push(wish);
+            }
+        }
+
+        const filteredWishesFilter = wishes.filter(wish => parseFloat(wish.price) > parseFloat(minimumPrice));
+
+        console.log(filteredWishesFilter);
+
+        renderWishes(filteredWishesFilter);
+    })
+
 }
 
 
